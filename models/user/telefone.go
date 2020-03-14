b5b7865f0bfaaa93b4301c2f5d218735988f5df8
matheus Calaça <matheusfcalaca@gmail.com>
2020-03-14 11:55:42 -0300

    Refatorado Model Telefone e adicionado descrcao do BD

--- models/user/telefone.go
+++ models/user/telefone.go
@@ -1,9 +1,9 @@
 package model
 
 type Telefone struct {
-	DDD       int64  `json:"ddd"`
-	Telefone  int64  `json:"telefone"`
-	Tipo      string `json:"tipo"`
+	DDD       int64  `db:"DDD" json:"ddd"`
+	Numero    int64  `db:"NUMERO" json:"numero"`
+	Tipo      string `db:"ITPO" json:"tipo"`
 	CreatedAt int64  `db:"CREATED_AT" json:"created_at"`
 	UpdatedAt int64  `db:"UPDATE_AT" json:"updated_at"`
 }
