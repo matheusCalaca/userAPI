c906c44c80f4f9eb6afaf67ab44e96e5adba8e51
matheus Calaça <matheusfcalaca@gmail.com>
2020-03-14 12:52:30 -0300

    Adicionado Model Pessoa

--- go.mod
+++ go.mod
@@ -7,7 +7,7 @@ require (
 	github.com/go-gorp/gorp v2.2.0+incompatible
 	github.com/go-playground/universal-translator v0.17.0 // indirect
 	github.com/go-sql-driver/mysql v1.5.0
-	github.com/golang/protobuf v1.3.5 // indirect
+	github.com/golang/protobuf v1.3.5
 	github.com/json-iterator/go v1.1.9 // indirect
 	github.com/leodido/go-urn v1.2.0 // indirect
 	github.com/lib/pq v1.3.0 // indirect
---
+++ models/user/pessoa.go
@@ -0,0 +1,17 @@
+package model
+
+import "time"
+
+type Pessoa struct {
+	ID_PESSOA      int64      `db:"ID_PESSOA" json:"id"`
+	CPF            int64      `db:"CPF" json:"CPF"`
+	Nome           string     `db:"NOME" json:"noem"`
+	Sobrenome      string     `db:"SOBRENOME" json:"sobrenome"`
+	RG             string     `db:"RG" json:"RG"`
+	DataNascimento time.Time  `db:"DATA_NASCIMETO" json:"dataNascimento"`
+	Email          string     `db:"EMAIL" json:"email"`
+	Telefone       []Telefone ` json:"telefones"`
+	Endereco       []Endereco ` json:"enderecos"`
+	CreatedAt      int64      `db:"CREATED_AT" json:"created_at"`
+	UpdatedAt      int64      `db:"UPDATE_AT" json:"updated_at"`
+}
