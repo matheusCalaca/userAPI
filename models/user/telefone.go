081be8f18c1d59fd5d6f8bc89a7e67783614ed0c
matheus Calaça <matheusfcalaca@gmail.com>
2020-03-14 11:42:06 -0300

    Adicionado Model de telefone

--- models/user/telefone.go
+++ models/user/telefone.go
@@ -1,7 +1,9 @@
 package model
 
 type Telefone struct {
-	DDD      int64  `json:"ddd"`
-	Telefone int64  `json:"telefone"`
-	Tipo     string `json:"tipo"`
+	DDD       int64  `json:"ddd"`
+	Telefone  int64  `json:"telefone"`
+	Tipo      string `json:"tipo"`
+	CreatedAt int64  `db:"CREATED_AT" json:"created_at"`
+	UpdatedAt int64  `db:"UPDATE_AT" json:"updated_at"`
 }
