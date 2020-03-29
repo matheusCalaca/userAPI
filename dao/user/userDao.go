fa32b3a7b82014e456a637c8a5d28e0ad4249515
matheus Calaça <matheusfcalaca@gmail.com>
2020-03-29 20:30:43 -0300

    Dicionado Metodo de UpperCase para o insert de pesso

--- dao/user/userDao.go
+++ dao/user/userDao.go
@@ -1,12 +1,14 @@
-package userDao
+package userdao
 
 import (
 	"log"
+	"strings"
 	models "userAPI/models/user"
 
 	"github.com/jinzhu/gorm"
 )
 
+// InserirPessoaBD insere a pessoa no banco de dados
 func InserirPessoaBD(pessoa models.Pessoa, db *gorm.DB) (models.Pessoa, error) {
 	// result, err := dbmap.Exec(`INSERT INTO pessoa(NOME, SOBRENOME, DATA_NASCIMENTO, CPF, RG, EMAIL, CREATED_AT) VALUES (?, ?, ?, ?, ?, ?, ?)`,
 	// 	pessoa.Nome, pessoa.Sobrenome, pessoa.DataNascimento, pessoa.CPF, pessoa.RG, pessoa.Email, pessoa.CreatedAt)
@@ -24,10 +26,30 @@ func InserirPessoaBD(pessoa models.Pessoa, db *gorm.DB) (models.Pessoa, error) {
 	// 	Tipo:   "CELULAR",
 	// }
 	// pessoa.CreatedAt = time.Now()
+	caseUpperBeforeSave(&pessoa)
 	result := db.Create(&pessoa)
 	log.Print(&result.Error)
 	return pessoa, nil
 
+}
+
+// caseUpperBeforeSave coloca em upper case al guns dados antes de salvar no banco
+func caseUpperBeforeSave(pessoa *models.Pessoa) {
+	for index, telefone := range pessoa.Telefone {
+		pessoa.Telefone[index].Tipo = strings.ToUpper(telefone.Tipo)
+	}
+
+	for index, endereco := range pessoa.Endereco {
+		pessoa.Endereco[index].Tipo = strings.ToUpper(endereco.Tipo)
+		pessoa.Endereco[index].Bairro = strings.ToUpper(endereco.Bairro)
+		pessoa.Endereco[index].Cidade = strings.ToUpper(endereco.Cidade)
+		pessoa.Endereco[index].Logradouro = strings.ToUpper(endereco.Logradouro)
+		pessoa.Endereco[index].Complemento = strings.ToUpper(endereco.Complemento)
+		pessoa.Endereco[index].UF = strings.ToUpper(endereco.UF)
+	}
+
+	pessoa.Nome = strings.ToUpper(pessoa.Nome)
+	pessoa.Sobrenome = strings.ToUpper(pessoa.Sobrenome)
 }
 
 // func SelectAllUser() (userModel.Users, error) {
