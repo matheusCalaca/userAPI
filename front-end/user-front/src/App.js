1dd54e0e6d37363b502f5f35bc91ded56f219196
matheus Calaça <matheusfcalaca@gmail.com>
2020-03-30 21:53:38 -0300

    Adicionado basico de formulario, axios, e basico service

--- front-end/user-front/src/App.js
+++ front-end/user-front/src/App.js
@@ -1,16 +1,23 @@
 import React from 'react';
 import './App.css';
+import api from './services/app'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-       
-        <p>
-          Hello world
-        </p>
-       
-      </header>
+    <div className="container">
+      <div className="container">
+
+        <h1>Cadastreo de Pessoa</h1>
+        <form htmlFor="cadastroPessoa">
+          <label htmlFor="nome">Nome:</label>
+          <input
+            type="text"
+            id="nome"
+            placeholder="Digite seu Nome"
+          />
+          <button type="submit">Entrar</button>
+        </form>
+      </div>
     </div>
   );
 }
