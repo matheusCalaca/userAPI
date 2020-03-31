21ee7eba8d58ffbdf7a30fd037b317076f7095e2
matheus Calaça <matheusfcalaca@gmail.com>
2020-03-30 22:06:20 -0300

    adicinado a evento de value do nome

--- front-end/user-front/src/App.js
+++ front-end/user-front/src/App.js
@@ -1,19 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
-import api from './services/app'
+// import api from './services/app'
 
 function App() {
+
+  const [nome, setNome] = useState('');
+
+  function handleSubmit(event) {
+    event.preventDefault()
+    console.log('Nome: ' + nome);
+
+    // values
+
+
+  }
+
+
+
   return (
     <div className="container">
       <div className="container">
 
         <h1>Cadastreo de Pessoa</h1>
-        <form htmlFor="cadastroPessoa">
+        <form onSubmit={handleSubmit}>
           <label htmlFor="nome">Nome:</label>
           <input
             type="text"
             id="nome"
             placeholder="Digite seu Nome"
+            value={nome}
+            onChange={event => setNome(event.target.value)}
           />
           <button type="submit">Entrar</button>
         </form>
