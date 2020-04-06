c1c8ab385cdb1ba4afc8834f2cd303e48800023f
matheus Calaça <matheusfcalaca@gmail.com>
2020-04-06 19:33:30 -0300

    Adicionado css provisorio para facilitar visualizacao

--- front-end/user-front/src/pages/cadastroPessoa/index.js
+++ front-end/user-front/src/pages/cadastroPessoa/index.js
@@ -1,8 +1,8 @@
-import React, { Component } from 'react';
 import { Form } from '@unform/web';
-import Input from '../../components/form/Input';
-import PhoneForm from '../../components/form/phone-form'
+import React, { Component } from 'react';
 import EnderecoForm from '../../components/form/endereco-form';
+import Input from '../../components/form/Input';
+import PhoneForm from '../../components/form/phone-form';
 
 
 
@@ -15,21 +15,45 @@ class CadastroPessoa extends Component {
     render() {
         return (
             <Form onSubmit={this.handleSubmit}>
-                <span>nome: </span> <br />
-                <Input name="nome" type="text" /> <br />
-                <span>sebrenome: </span> <br />
-                <Input name="sobrenome" type="text" /> <br />
-                <span>CPF: </span> <br />
-                <Input name="CPF" type="text" /> <br />
-                <span>RG: </span> <br />
-                <Input name="RG" type="text" /> <br />
-                <span>dataNascimento: </span> <br />
-                <Input name="dataNascimento" type="date" /> <br />
-                <span>email: </span> <br />
-                <Input name="email" type="email" /> <br />
-                <PhoneForm ></PhoneForm>
-                <EnderecoForm></EnderecoForm>
+                <div className="contentForm">
+                    <div className="contentFornInsider">
+                        <div>
+                            <span>nome: </span> <br />
+                            <Input name="nome" type="text" /> <br />
+                        </div>
+                        <div>
+                            <span>sebrenome: </span> <br />
+                            <Input name="sobrenome" type="text" /> <br />
+                        </div>
+                        <div>
+                            <span>CPF: </span> <br />
+                            <Input name="CPF" type="text" /> <br />
+                        </div>
+                        <div>
+                            <span>RG: </span> <br />
+                            <Input name="RG" type="text" /> <br />
+                        </div>
+                        <div>
+                            <span>dataNascimento: </span> <br />
+                            <Input name="dataNascimento" type="date" /> <br />
+                        </div>
+                        <div>
+                            <span>email: </span> <br />
+                            <Input name="email" type="email" /> <br />
+                        </div>
+                    </div>
 
+                    <div lassName="contentFornInsider">
+                        <div>
+                            <PhoneForm ></PhoneForm>
+                        </div>
+                    </div>
+                    <div lassName="contentFornInsider">
+                        <div>
+                            <EnderecoForm></EnderecoForm>
+                        </div>
+                    </div>
+                </div>
                 <button type="submit">Cadastrar</button>
             </Form>
         );
