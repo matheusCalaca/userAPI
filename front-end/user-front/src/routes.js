09a82466b5d50d00b15f68e4191fcb06c6f0d568
matheus Calaça <matheusfcalaca@gmail.com>
2020-04-11 19:12:56 -0300

    Adicionado template de exmplo para o react

--- front-end/user-front/src/routes.js
+++ front-end/user-front/src/routes.js
@@ -3,6 +3,7 @@ import  {BrowserRouter, Switch, Route} from 'react-router-dom';
 
 import CadastroPessoa from './pages/cadastroPessoa';
 import ListaPessoa from './pages/listaPessoa';
+import Dashboard from './pages/dashboard/Dashboard';
 
 
 export default function Routes(){
@@ -10,6 +11,7 @@ export default function Routes(){
         <BrowserRouter>
         <Switch>
             <Route path="/" exact component={CadastroPessoa}/>
+            <Route path="/teste" exact component={Dashboard}/>
             <Route path="/listar" component={ListaPessoa}/>
         </Switch>
         </BrowserRouter>
