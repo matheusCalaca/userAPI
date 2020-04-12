3c19ef4f7308fc2292681d2363c823cb4c92ff43
matheus Calaça <matheusfcalaca@gmail.com>
2020-04-11 21:41:19 -0300

    alterado para componete em vez de function o react

--- front-end/user-front/src/pages/pessoa/listarPessoa.js
+++ front-end/user-front/src/pages/pessoa/listarPessoa.js
@@ -1,13 +1,8 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TablePagination from '@material-ui/core/TablePagination';
-import TableRow from '@material-ui/core/TableRow';
+import { Paper, TableRow, TablePagination, TableHead, Table, TableBody, TableCell, TableContainer } from '@material-ui/core';
+import ReactDOM from 'react-dom';
+
 
 const columns = [
     { id: 'nome', label: 'Nome', minWidth: 50 },
@@ -38,10 +33,9 @@ const useStyles = makeStyles({
     },
 });
 
-export default function ListaPessoa() {
-    const classes = useStyles();
-    const [page, setPage] = React.useState(0);
-    const [rowsPerPage, setRowsPerPage] = React.useState(10);
+
+
+class ListaPessoa extends Component {
 
     //   const handleChangePage = (event, newPage) => {
     //     setPage(newPage);
@@ -51,51 +45,65 @@ export default function ListaPessoa() {
     //     setRowsPerPage(+event.target.value);
     //     setPage(0);
     //   };
+    constructor(props) {
+        super(props);
+        this.state = {
+            classes: useStyles,
+            page: 0,
+            setPage:0,
+            rowsPerPage:10,
+            setRowsPerPage:10
+          };
+    }
 
-    return (
-        <Paper className={classes.root}>
-            <TableContainer className={classes.container}>
-                <Table stickyHeader aria-label="sticky table">
-                    <TableHead>
-                        <TableRow>
-                            {columns.map((column) => (
-                                <TableCell
-                                    key={column.id}
-                                    align={column.align}
-                                    style={{ minWidth: column.minWidth }}
-                                >
-                                    {column.label}
-                                </TableCell>
-                            ))}
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row) => {
-                            return (
-                                <TableRow hover role="checkbox" tabIndex={-1} key={row.code}>
-                                    {columns.map((column) => {
-                                        const value = row[column.id];
-                                        return (
-                                            <TableCell key={column.id} align={column.align}>
-                                                {column.format && typeof value === 'number' ? column.format(value) : value}
-                                            </TableCell>
-                                        );
-                                    })}
-                                </TableRow>
-                            );
-                        })}
-                    </TableBody>
-                </Table>
-            </TableContainer>
-            <TablePagination
-                // rowsPerPageOptions={[10, 25, 100]}
-                component="div"
-            // count={rows.length}
-            // rowsPerPage={rowsPerPage}
-            // page={page}
-            // onChangePage={handleChangePage}
-            // onChangeRowsPerPage={handleChangeRowsPerPage}
-            />
-        </Paper>
-    );
+    render() {
+
+        return (
+            <Paper className={this.state.classes.root}>
+                <TableContainer className={this.state.classes.container}>
+                    <Table stickyHeader aria-label="sticky table">
+                        <TableHead>
+                            <TableRow>
+                                {columns.map((column) => (
+                                    <TableCell
+                                        key={column.id}
+                                        align={column.align}
+                                        style={{ minWidth: column.minWidth }}
+                                    >
+                                        {column.label}
+                                    </TableCell>
+                                ))}
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {rows.slice(this.state.page * this.state.rowsPerPage, this.state.page * this.state.rowsPerPage + this.state.rowsPerPage).map((row) => {
+                                return (
+                                    <TableRow hover role="checkbox" tabIndex={-1} key={row.code}>
+                                        {columns.map((column) => {
+                                            const value = row[column.id];
+                                            return (
+                                                <TableCell key={column.id} align={column.align}>
+                                                    {column.format && typeof value === 'number' ? column.format(value) : value}
+                                                </TableCell>
+                                            );
+                                        })}
+                                    </TableRow>
+                                );
+                            })}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+                <TablePagination
+                    // rowsPerPageOptions={[10, 25, 100]}
+                    component="div"
+                // count={rows.length}
+                // rowsPerPage={rowsPerPage}
+                // page={page}
+                // onChangePage={handleChangePage}
+                // onChangeRowsPerPage={handleChangeRowsPerPage}
+                />
+            </Paper>
+        );
+    }
 }
+export default ListaPessoa; 
