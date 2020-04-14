9e9d4432437320df66d3080240202053864cafd6
matheus Calaça <matheusfcalaca@gmail.com>
2020-04-13 21:22:38 -0300

    reaftorado estrutura de dashboard

--- front-end/user-front/src/App.js
+++ front-end/user-front/src/App.js
@@ -130,7 +130,6 @@ function App() {
     const handleDrawerClose = () => {
         setOpen(false);
     };
-    const fixedHeightPaper = clsx(classes.paper, classes.fixedHeight);
 
 
     return (
@@ -186,21 +185,12 @@ function App() {
             <main className={classes.content}>
                 <div className={classes.appBarSpacer} />
                 <Container maxWidth="lg" className={classes.container}>
-                    <Routes />
                     <Grid container spacing={3}>
-                        
-                        {/* Chart */}
-                        {/* <Grid item xs={12} md={8} lg={9}>
-                            <Paper className={fixedHeightPaper}>
-                                <Chart />
+                        <Grid item xs={12}>
+                            <Paper className={classes.paper}>
+                                <Routes />
                             </Paper>
-                        </Grid> */}
-                        {/* Recent Deposits */}
-                        {/* <Grid item xs={12} md={4} lg={3}>
-                            <Paper className={fixedHeightPaper}>
-                                <Deposits />
-                            </Paper>
-                        </Grid> */}
+                        </Grid>
 
                     </Grid>
                     <Box pt={4}>
--- front-end/user-front/src/routes.js
+++ front-end/user-front/src/routes.js
@@ -2,17 +2,16 @@ import React from 'react';
 import  {BrowserRouter, Switch, Route} from 'react-router-dom';
 
 import CadastroPessoa from './pages/cadastroPessoa';
-import ListaPessoa from './pages/listaPessoa';
-import Dashboard from './pages/dashboard/Dashboard';
+import listarPessoa from './pages/pessoa/listarPessoa';
 
 
 export default function Routes(){
     return(
         <BrowserRouter>
         <Switch>
-            <Route path="/" exact component={Dashboard}/>
+            <Route path="/" exact component={listarPessoa}/>
             <Route path="/cadastrar" component={CadastroPessoa}/>
-            <Route path="/listar" component={ListaPessoa}/>
+            <Route path="/listar" component={listarPessoa}/>
         </Switch>
         </BrowserRouter>
     )
